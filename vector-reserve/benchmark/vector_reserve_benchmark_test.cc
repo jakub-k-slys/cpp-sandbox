8db39c7b178ec51073421bd0b18194bdc5af704f
slysj <Jakub.Slys@pega.com>
2023-04-13 18:31:55 +0200

    testing an impact of reserve method of vector

--- vector-reserve/benchmark/vector_reserve_benchmark_test.cc
+++ vector-reserve/benchmark/vector_reserve_benchmark_test.cc
@@ -1,10 +1,27 @@
 #include "benchmark/benchmark.h"
+#include <vector>
 
-
-static void vector_resize_benchmark_test(benchmark::State& state) {
+static void vector_push_back_benchmark_test(benchmark::State& state) {
+  int no_of_elements = state.range(0);
+  std::vector<int> longs;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(0);
+    for(int i = 0; i < no_of_elements; ++i) {
+      longs.push_back(no_of_elements);
+    }
   }
 }
 
-BENCHMARK(vector_resize_benchmark_test)->RangeMultiplier(2)->Range(1, 100);
+BENCHMARK(vector_push_back_benchmark_test)->RangeMultiplier(2)->Range(1, 10000000);
+
+static void vector_reserve_before_push_back_benchmark_test(benchmark::State& state) {
+  int no_of_elements = state.range(0);
+  std::vector<int> longs;
+  longs.reserve(no_of_elements);
+  for (auto _ : state) {
+    for(int i = 0; i < no_of_elements; ++i) {
+      longs.push_back(no_of_elements);
+    }
+  }
+}
+
+BENCHMARK(vector_reserve_before_push_back_benchmark_test)->RangeMultiplier(2)->Range(1, 10000000);
